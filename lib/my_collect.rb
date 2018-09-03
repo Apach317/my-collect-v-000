7bf1904ad628105c06a63d996d1acf5dd93f586c
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 02:32:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,8 @@
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+
 def my_collect(languages)
   collect = []
-  while collect(languages) < languages.length do
+  while collect < languages.length do |i|
     languages.upcase
   end
 end
