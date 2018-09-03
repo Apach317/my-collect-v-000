953e5546c0e6de02ab12d013a1009628895c5938
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 02:35:26 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,21 @@
 languages = ['ruby', 'javascript', 'python', 'objective-c']
 
 def my_collect(languages)
+  num = 0
   collect = []
-  while collect < languages.length do |i|
-    languages.upcase
+  while collect < languages.length
+    collect << (yield languages[num])
+    num += 1
   end
+  collect
+end
+
+
+i = 0
+ new_arg = []
+ while i < arg.length
+   new_arg.push(yield arg[i])
+   i += 1
+ end
+ new_arg
 end
