bd6aa133182f3c0679a313738e42a81097d4d800
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 02:30:35 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
 def my_collect(languages)
   collect = []
-  while collect(languages) < languages.length do |language|
+  while collect(language) < languages.length do |language|
     language.upcase
   end
 end
